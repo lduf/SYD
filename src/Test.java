20237d6d4fc8d6d36d6a87204586df816bad5d96
Dufour Lucas <ldufour2@tc405-009-03.insa-lyon.fr>
2021-09-29 09:00:08 +0200

    simplification des classes

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import java.net.*;
 class Test{
 	public static void main(String arg[]){
+		final int PORT = 5000;
+		try{
+				ServerSocket connection = new ServerSocket(PORT);
+				Socket socket = connection.accept();
+				System.out.println("Client : "+socket.getInetAddress());
+
+				BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+
+				String line = in.readLine();
+				while (line  != null){
+					System.out.println(line);
+					line = in.readLine();
+				}
+				in.close();
+			}catch(IOException e){
+				System.err.println("I/O erreur");
+			}
+		
+		
 		Point p1 = new Point(0,1); // Creates a new point with x=0 and y=1
 		Point p2 = new Point(); 
 
